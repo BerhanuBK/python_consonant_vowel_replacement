d47a99be23a295f1d58762b6d04c4cd7ce8f91aa
Berhanu Kelecha <bkelecha@ndi.org>
2022-11-04 16:59:19 -0400

    Updated the replacement code to have functions for both consonant and vowel

--- stringman.py
+++ stringman.py
@@ -1,17 +1,39 @@
 con = ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
-def str_replace(word):
+vwl = ['a', 'e', 'i', 'o', 'u']
+def con_str_replace(word):
 	length = len(con)
-	for x in word:
-		#print(x, word, length)
+	word = word.lower()
+	word2 = [x for x in word]
+	#print(word, word2)
+	for c in range(len(word)):
+		x=word[c]
 		if x in con:
-			#print(x, con.index(x))
 			if con.index(x) == (length - 1): 
-				word = word.replace(x, con[(con.index(x)+1)-length]) 
-				#print(word)
+				word2[c] = con[(con.index(x)+1)-length] 
 			else:
-				word = word.replace(x, con[(con.index(x)+1)])
-				#print(word)
-	return(word)
+				word2[c] = con[con.index(x)+1]
+	return("".join(word2))
 
-print(str_replace("Abebe")) 
-print(str_replace("zZZei"))  
+def vow_str_replace(word):
+	length = len(vwl)
+	word = word.lower()
+	word2 = [x for x in word]
+	for c in range(len(word)):
+		x=word[c]
+		if x in vwl:
+			if vwl.index(x) == (length - 1): 
+				word2[c] = vwl[(vwl.index(x)+1)-length]
+			else:
+				word2[c] = vwl[vwl.index(x)+1]
+	return("".join(word2))
+print("----------------------------------------------------")
+print("Sample consonant replacement")
+print("DAbebe ==>", con_str_replace("DAbebe"))
+print("Abebc ==>", con_str_replace("Abebc")) 
+print("zZZei ==>", con_str_replace("zZZei"))
+print("----------------------------------------------------")
+print("Sample vowel replacement")
+print("Abebe ==>", vow_str_replace("Abebe")) 
+print("zuZZei ==>", vow_str_replace("zuZZei"))
+
+  
